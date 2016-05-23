b1b709326cd24a53e81668c954c0b9e6549404fc
sai <sai@csdtp-036.(none)>
2016-05-23 16:48:39 +0530

    lang complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -28,6 +28,7 @@ class HomeController < ApplicationController
 				@id = params[:select]
 				@lang = params[:lang]
 				@language=@languages.where("name=?",@lang)
+
 				if @id=="0"
 					if @lang == "all"
 						@feeds=Feed.where("id  in (select feed_id from subscribes where user_id=?)",current_member.id)
@@ -51,12 +52,52 @@ class HomeController < ApplicationController
 
 	def list
 		@categories=Category.all
+		@languages = Language.all
+		if member_signed_in?
+			if params[:lang].nil? and params[:select].nil?
+				@feeds=Feed.where("id  not in (select feed_id from subscribes where user_id=?)",current_member.id)
+			elsif params[:lang]==""
+				@id = params[:select]
+				
+				if @id=="0" or @id=="" or @id.nil?
 
-		@id = params[:select]
-		if @id.nil?
-			@feeds=Feed.where("id not in (select feed_id from subscribes where user_id=?)",current_member.id)
+					@feeds=Feed.where("id not in (select feed_id from subscribes where user_id=?)",current_member.id)
+				else
+					@feeds=Feed.where("c_id = ? and id not in (select feed_id from subscribes where user_id=?)", @id,current_member.id)	
+				end
+
+			elsif params[:select]==""
+				@lang = params[:lang]
+
+				if @lang=="all" or @lang==""
+					@feeds=Feed.where("id not in (select feed_id from subscribes where user_id=?)",current_member.id)
+				else
+					@language=@languages.where("name=?",@lang)
+					@feeds=Feed.where("lang_id = ? and id not in (select feed_id from subscribes where user_id=?)", @language.ids[0],current_member.id)	
+				end
+			else
+				@id = params[:select]
+				@lang = params[:lang]
+				@language=@languages.where("name=?",@lang)
+
+				if @id=="0"
+					if @lang == "all"
+						@feeds=Feed.where("id not in (select feed_id from subscribes where user_id=?)",current_member.id)
+					else
+						@feeds=Feed.where("lang_id = ? and id not in (select feed_id from subscribes where user_id=?)", @language.ids[0],current_member.id)	
+					end	
+				else
+					if @lang == "all"	
+						@feeds=Feed.where("c_id = ? and id not in (select feed_id from subscribes where user_id=?)", @id,current_member.id)	
+					else
+
+						@feeds=Feed.where("c_id = ? and id not in (select feed_id from subscribes where user_id=? and lang_id=?)", @id,current_member.id,@language.ids[0])
+
+					end
+				end
+			end
 		else
-			@feeds=Feed.where("c_id = ? and id not in (select feed_id from subscribes where user_id=?)", @id,current_member.id)	
+			redirect_to url_for(:controller => :members, :action => :sign_up)
 		end
 
 	end
